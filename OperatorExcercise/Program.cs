cbe90b16ef85b06e1b0c0e5c255808cb99a48430
Hy Huynh <hyxhuynh@gmail.com>
2020-10-26 09:46:31 -0700

    added a thought excercise

--- OperatorExcercise/Program.cs
+++ OperatorExcercise/Program.cs
@@ -29,15 +29,24 @@ namespace OperatorExcercise
             int b = 4;
             int quotient = a / b;
             int remainder = a % b;
-            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}.");
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}.\n");
 
             // Calculate Area of Circle using user input
             Console.WriteLine("What is the radius of the circle?");
             double radius = double.Parse(Console.ReadLine());
             double areaResult = AreaOfCircle(radius);
-            Console.WriteLine($"The area of a circle with radius {radius} is {areaResult}.");
+            Console.WriteLine($"The area of a circle with radius {radius} is {areaResult}.\n");
 
 
+            // Thought Excercise
+            Console.WriteLine("What is the value of k in the following code?\n" +
+                "> var i = 3;\n> var j = 4;\n> var k = ++i * j++;\n> Console.WriteLine(k); ");
+            var answer = Console.ReadLine();
+            
+            var i = 3;
+            var j = 4;
+            var k = ++i * j++;
+            Console.WriteLine($"The correct answer is {k}.");
         }
     }
 }
