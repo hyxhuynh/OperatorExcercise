b0e2d6ee0b1c636762bfd1166fe84802063673f1
Hy Huynh <hyxhuynh@gmail.com>
2020-10-26 08:55:06 -0700

    initial commit

---
+++ OperatorExcercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OperatorExcercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 8;
+            int y = 7;
+
+            int addition = x + y;
+            Console.WriteLine($"{x}+{y} is {addition}.");
+
+            int substraction = x - y;
+            Console.WriteLine($"{x}-{y} is {substraction}.");
+
+            int multiplication = x * y;
+            Console.WriteLine($"{x}*{y} is {multiplication}.");
+
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}.");
+        }
+    }
+}
